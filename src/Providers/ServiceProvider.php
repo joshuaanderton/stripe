8f58b3a37584aee15d29944a8e344745952f6127
Joshua Anderton <info@joshanderton.com>
2024-03-25 18:44:41 -0700

    Optimize

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -3,12 +3,13 @@
 namespace Ja\Stripe\Providers;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Ja\Stripe\Support\StripeCustomer;
 
 class ServiceProvider extends BaseServiceProvider
 {
     public function register()
     {
-        //
+        $this->app->singleton('stripe-customer', fn () => new StripeCustomer);
     }
 
     public function boot()
