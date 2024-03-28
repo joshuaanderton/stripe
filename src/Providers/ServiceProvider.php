0852ca6a15dbc95f001626e4ce93608cb83c4c8f
Joshua Anderton <info@joshanderton.com>
2024-03-27 23:33:23 -0700

    Fix model binding

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -3,13 +3,12 @@
 namespace Ja\Stripe\Providers;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
-use Ja\Stripe\Support\StripeCustomer;
 
 class ServiceProvider extends BaseServiceProvider
 {
     public function register()
     {
-        $this->app->singleton('stripe-customer', fn () => new StripeCustomer);
+        //
     }
 
     public function boot()
